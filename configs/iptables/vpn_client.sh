1a6f17e1123ecf36ded55036029d592d6b48bf38
Vincenzo Auteri <vincenzo.auteri@hotmail.com>
2014-05-20 02:10:55 +0200

    open vpn output

--- configs/iptables/vpn_client.sh
+++ configs/iptables/vpn_client.sh
@@ -6,7 +6,7 @@ iptables -X
 
 # Setting default filter policy
 iptables -P INPUT DROP
-iptables -P OUTPUT DROP
+iptables -P OUTPUT ACCEPT 
 iptables -P FORWARD DROP
 
 # Allow unlimited traffic on loopback
@@ -25,19 +25,10 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 #allow port 1194 udp used by vpn 
 iptables -A INPUT -p tcp --dport 1194 -j ACCEPT
 
-#OUTBOUND CONNECTIONS 
-
-#allow SSH  on port 22, (established connections auto accepted)
-iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
-
-#allow DNS 
-iptables -A OUTPUT -p tcp --dport 53  -j ACCEPT
-iptables -A OUTPUT -p udp --dport 53  -j ACCEPT
-
 
 #allow port 1194 udp used by vpn 
 iptables -A OUTPUT -p tcp --dport 1194 -j ACCEPT
 
 #drop anything that doesnt match the rules above
 iptables -A INPUT -j DROP
-iptables -A OUTPUT -j DROP
+iptables -A OUTPUT -j ACCEPT 
--- configs/iptables/vpn_server.sh
+++ configs/iptables/vpn_server.sh
@@ -6,8 +6,8 @@ iptables -X
 
 # Setting default filter policy
 iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT 
+iptables -P FORWARD ACCEPT 
 
 # Allow unlimited traffic on loopback
 iptables -A INPUT -i lo -j ACCEPT
@@ -22,27 +22,9 @@ iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 #allow SSH  on port 22, (established connections auto accepted)
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-#allow port 1194 udp used by vpn 
-iptables -A INPUT -p udp --dport 1194 -j ACCEPT
+#allow port 1194 tcp used by vpn 
+iptables -A INPUT -p tcp --dport 1194 -j ACCEPT
 
-#OUTBOUND CONNECTIONS 
-
-#allow SSH  on port 22, (established connections auto accepted)
-iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
-
-#allow DNS 
-iptables -A OUTPUT -p tcp --dport 53  -j ACCEPT
-iptables -A OUTPUT -p udp --dport 53  -j ACCEPT
-
-#allow http and https 
-iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
-
-#allow ftp 
-iptables -A OUTPUT -p tcp --dport 21 -j ACCEPT
-
-#allow port 1194 udp used by vpn 
-iptables -A OUTPUT -p udp --dport 1194 -j ACCEPT
 
 #NAT rules
 #VPN reorute remote ->local
@@ -50,4 +32,4 @@ iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j SNAT --to-source 192.16
 
 #drop anything that doesnt match the rules above
 iptables -A INPUT -j DROP
-iptables -A OUTPUT -j DROP
+iptables -A OUTPUT -j ACCEPT 
